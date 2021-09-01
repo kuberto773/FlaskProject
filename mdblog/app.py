168c3d3f463eb09a56d910a5c84f775124d96503
Jakub <krkoska.jakub1@gmail.com>
2021-09-01 10:07:26 +0000

    Using jinja template system

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from .database import articles
 
 flask_app = Flask(__name__)
 
@@ -13,8 +14,21 @@ def view_about_page():
 
 @flask_app.route("/articles/")
 def view_articles_page():
-    return render_template("articles_page.jinja")
+    return render_template("articles_page.jinja", articles = articles.items())
 
 @flask_app.route("/admin/")
 def view_admin_page():
     return render_template("admin_page.jinja")
+
+@flask_app.route("/article/<int:article_id>")
+def view_article_page(article_id):
+    article = articles.get(article_id)
+    if article:
+        return render_template("article_page.jinja", article = article)
+    else:
+        return render_template("article_not_found.jinja",article_id = article_id)
+
+@flask_app.route("/login/")
+def view_login_page():
+    return render_template("login_page.jinja")
+
