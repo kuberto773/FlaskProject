705b8bde1dca79275be3f4443eedb0db076c8df7
Jakub <krkoska.jakub1@gmail.com>
2021-09-01 09:03:13 +0000

    basic page layout and navigation

--- mdblog/app.py
+++ mdblog/app.py
@@ -4,11 +4,17 @@ from flask import render_template
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
-def index():
+def view_welcome_page():
     return render_template("welcome_page.jinja")
 
-
 @flask_app.route("/about/")
-def about():
+def view_about_page():
     return render_template("about_page.jinja")
 
+@flask_app.route("/articles/")
+def view_articles_page():
+    return render_template("articles_page.jinja")
+
+@flask_app.route("/admin/")
+def view_admin_page():
+    return render_template("admin_page.jinja")
